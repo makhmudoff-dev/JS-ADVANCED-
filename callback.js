824a47b125a4419678dd7e09709bc0a2c9502f58
makhmudoff <gachon031698@gmail.com>
2023-03-30 05:38:25 +0900

    created async, promise, callback js files

---
+++ callback.js
@@ -0,0 +1,5 @@
+const users = [
+  { name: "John", lastName: "Hooly", email: "john@example.com" },
+  { name: "Muhammad", lastName: "Mali", email: "mali@example.com" },
+  { name: "Dilbar", lastName: "Dilbar", email: "dilbar@example.com" },
+];
